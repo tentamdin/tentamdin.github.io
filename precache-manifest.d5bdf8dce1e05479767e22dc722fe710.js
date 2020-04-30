179e351e0f9b1d2dc0db3f861e3d826d3e0f17ea
Tenzin Tamdin <38410384+tentamdin@users.noreply.github.com>
2020-04-30 12:47:24 +0530

    Updates

---
+++ precache-manifest.d5bdf8dce1e05479767e22dc722fe710.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52295ce0dc198376873f33b75c5a96c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4a863c75791e52fe475",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ec42395ce2d579ca7862",
+    "url": "/static/css/main.cffc6c5f.chunk.css"
+  },
+  {
+    "revision": "e4a863c75791e52fe475",
+    "url": "/static/js/2.bb83cb10.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.bb83cb10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec42395ce2d579ca7862",
+    "url": "/static/js/main.c6d0b693.chunk.js"
+  },
+  {
+    "revision": "f82d0168e3d82708fb03",
+    "url": "/static/js/runtime-main.7e0ae243.js"
+  },
+  {
+    "revision": "59b66fe6f1834a6f3be5580fff7306f0",
+    "url": "/static/media/Me.59b66fe6.jpeg"
+  },
+  {
+    "revision": "c3242a008199e931f1451eaf6a443a63",
+    "url": "/static/media/cs2.c3242a00.jpg"
+  }
+]);
